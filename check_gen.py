9e5766908cb89fcd85ed2bf73098f5e669727944
Luca Casati <luk00casati@gmail.com>
2025-06-03 21:30:17 +0200

    simplify

--- check_gen.py
+++ check_gen.py
@@ -3,35 +3,32 @@ import os
 from collections import Counter
 
 redirect_script = """
-    <!-- redirect -->
-    <script>
-      // If the current path is exactly "/"
-      if (window.location.pathname === "/") {
+<!-- redirect -->
+<script>
+    // If the current path is exactly "/"
+    if (window.location.pathname === "/") {
         window.location.replace("/bello.html");
-      }
-    </script>
+    }
+</script>
 """
 
 header = """
 <!doctype html>
 <html lang="it">
-  <head>
-    <script
-      src="https://cdn.counter.dev/script.js"
-      data-id="6201f173-1697-4253-bf68-83e025b07874"
-      data-utcoffset="2"
-    ></script>
-    <link rel="stylesheet" href="style.css" />
-    <link rel="icon" href="siteres/icon/favicon.svg" type="image/svg+xml" />
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<head>
+<link rel="stylesheet" href="style.css" />
+<link rel="icon" href="siteres/icon/favicon.svg" type="image/svg+xml" />
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 """
 
 link = '''
-      <a href="bello.html">BELLO</a>
-      <a href="mhe.html">MHE</a>
-      <a href="brutto.html">BRUTTO</a>
-      <a href="index.html">TUTTO</a>
+<nav>
+<a href="bello.html">BELLO</a>
+<a href="mhe.html">MHE</a>
+<a href="brutto.html">BRUTTO</a>
+<a href="index.html">TUTTO</a>
+</nav>
 '''
 
 directory = "opere"
@@ -94,73 +91,65 @@ with open("index.html", "w") as f:
     f.write(header)
     f.write(redirect_script)
     f.write(
-        """
+    """
     <title>Galleria delle mie opere artistiche</title>
-  </head>
-
-  <body>
-      <div class="container">
-      <h1>Galleria delle mie opere artistiche</h1>
+    </head>
+    <body>
+    <h1>Galleria delle mie opere artistiche</h1>
     """
     )
     f.write(link)
     for all in all_grouped:
         name_without_ext = os.path.splitext(all)[0]
-        f.write(f'<img src="{directory}/{all}" alt="{name_without_ext}" />')
-    f.write("</div>\n</body>\n</html>")
+        f.write(f'<img src="{directory}/{all}" alt="{name_without_ext}" />\n')
+    f.write("</body>\n</html>")
 
 with open("bello.html", "w") as f:
     f.write(header)
     f.write(
-        """
+    """
     <title>Galleria delle mie opere artistiche belle</title>
-  </head>
-
-  <body>
-          <div class="container">
-      <h1>Galleria delle mie opere artistiche belle</h1>
+    </head>
+    <body>
+    <h1>Galleria delle mie opere artistiche belle</h1>
     """
     )
     f.write(link)
     for bello in bello_img:
         name_without_ext = os.path.splitext(bello)[0]
-        f.write(f'<img src="{directory}/{bello}" alt="{name_without_ext}" />')
-    f.write("</div>\n</body>\n</html>")
+        f.write(f'<img src="{directory}/{bello}" alt="{name_without_ext}" />\n')
+    f.write("</body>\n</html>")
 
 with open("mhe.html", "w") as f:
     f.write(header)
     f.write(
-        """
+    """
     <title>Galleria delle mie opere artistiche mhe</title>
-  </head>
-
-  <body>
-         <div class="container">
-      <h1 >Galleria delle mie opere artistiche mhe</h1>
+    </head>
+    <body>
+    <h1 >Galleria delle mie opere artistiche mhe</h1>
     """
     )
     f.write(link)
     for mhe in mhe_img:
         name_without_ext = os.path.splitext(mhe)[0]
-        f.write(f'<img src="{directory}/{mhe}" alt="{name_without_ext}" />')
-    f.write("</div>\n</body>\n</html>")
+        f.write(f'<img src="{directory}/{mhe}" alt="{name_without_ext}" />\n')
+    f.write("</body>\n</html>")
 
 with open("brutto.html", "w") as f:
     f.write(header)
     f.write(
-        """
+    """
     <title>Galleria delle mie opere artistiche brutte</title>
-  </head>
-
-  <body>
-         <div class="container">
-      <h1>Galleria delle mie opere artistiche brutte</h1>
+    </head>
+    <body>
+    <h1>Galleria delle mie opere artistiche brutte</h1>
     """
     )
     f.write(link)
     for brutto in brutto_img:
         name_without_ext = os.path.splitext(brutto)[0]
-        f.write(f'<img src="{directory}/{brutto}" alt="{name_without_ext}" />')
-    f.write("</div>\n</body>\n</html>")
+        f.write(f'<img src="{directory}/{brutto}" alt="{name_without_ext}" />\n')
+    f.write("</body>\n</html>")
 
 print("DONE GENERATE")
